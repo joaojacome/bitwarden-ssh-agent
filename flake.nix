cddc608081f428e27baa269025bf4464b61415fd
Joao Jacome <github@joaojaco.me>
2023-08-08 13:01:06 +0100

    nix flake update

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         let
           package = pkgs.python3Packages.buildPythonPackage {
             pname = "bitwarden-ssh-agent";
-            version = "0.1.1";
+            version = "0.1.2";
             src = ./. ;
             propagatedBuildInputs = [ pkgs.python3Packages.setuptools pkgs.bitwarden-cli ];
             format = "other";
